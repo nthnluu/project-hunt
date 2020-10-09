f698f6428999bcf25541ba496c392bc579691708
Kevin Hsu <kevin.hsukaihao@gmail.com>
2020-10-09 12:35:36 -0700

    Basic profile info display

--- pages/Profile/[profile_id].js
+++ pages/Profile/[profile_id].js
@@ -5,7 +5,7 @@ import AuthContext from "../../src/AuthContext";
 import {useRouter} from "next/router";
 import PageLayout from "../../components/PageLayout";
 import ArrayInputPanel from "../../components/modals/NewProjectModal/ArrayInputPanel";
-import { Container, Paper, Box } from '@material-ui/core'
+import { Chip, Container, Paper, Box } from '@material-ui/core'
 
 import GitHubIcon from '@material-ui/icons/GitHub';
 import TwitterIcon from '@material-ui/icons/Twitter';
@@ -28,52 +28,57 @@ export default function () {
     // skills: skillArray
 
     useEffect(() => {
-      fb.firestore().collection("profiles").doc(profile_id)
-        .onSnapshot(function(doc) {
-          setPageData(doc.data())
-        });
+        fb.firestore().collection("profiles").doc(profile_id)
+            .onSnapshot(function(doc) {
+                setPageData(doc.data())
+            });
     });
 
     return <PageLayout>
-      <Container maxWidth="md" className="my-24">
-        <h1 className="mb-6 text-3xl font-display ">Basic Information</h1>
-        <div className="space-y-4">
-          <Paper variant="outlined">
-            {pageData
-              ? <Box p={4}>
+        <Container maxWidth="md" className="my-24">
+            <h1 className="mb-6 text-3xl font-display ">Basic Information</h1>
+            <div className="space-y-4">
+                <Paper variant="outlined">
+                    {pageData
+                        ? <Box p={4}>
 
-                <h2 className="text-xl font-display">Bio: </h2>
-                <p>{ pageData.bio } </p>
+                            <div className="newProjectFormPanelGrid">
+                                <div className="grid grid-cols-4">
+                                    { pageData.githubUrl
+                                        ? <a href={pageData.githubUrl}><GitHubIcon fontSize="large"/></a>
+                                        : <p/> }
 
-                <div className="grid grid-cols-4">
-                  { pageData.githubUrl
-                    ? <a href={pageData.githubUrl}><GitHubIcon fontSize="large"/></a>
-                    : <p/> }
+                                    { pageData.twitterUrl
+                                        ? <a href={pageData.twitterUrl}><TwitterIcon fontSize="large"/></a>
+                                        : <p/> }
 
-                  { pageData.twitterUrl
-                    ? <a href={pageData.twitterUrl}><TwitterIcon fontSize="large"/></a>
-                    : <p/> }
+                                    { pageData.websiteUrl
+                                        ? <a href={pageData.websiteUrl}><WebIcon fontSize="large"/></a>
+                                        : <p/> }
 
-                  { pageData.websiteUrl
-                    ? <a href={pageData.websiteUrl}><WebIcon fontSize="large"/></a>
-                    : <p/> }
+                                    { pageData.linkedinUrl
+                                        ? <a href={pageData.linkedinUrl}><LinkedInIcon fontSize="large"/></a>
+                                        : <p/> }
+                                </div>
 
-                  { pageData.linkedinUrl
-                    ? <a href={pageData.linkedinUrl}><LinkedInIcon fontSize="large"/></a>
-                    : <p/> }
-                </div>
+                                <div>
+                                    <h2 className="text-xl font-display">Bio: </h2>
+                                    <p>{ pageData.bio } </p>
+                                </div>
 
-                <div className="w-full">
-                  {pageData.skills.map((skill, index) =>
-                  <Chip key={skill.id}
-                    label={skill.value} variant="outlined" className="mr-2 mb-2"/>)}
-                </div>
+                                <div className="w-full">
+                                    { pageData.skills.map((skill, index) =>
+                                            <Chip key={skill.id}
+                                                label={skill.value} variant="outlined" className="mr-2 mb-2"/>)
+                                    }
+                                </div>
+                            </div>
 
-                </Box>
-              : <Box/>
-            }
-          </Paper> 
-          </div>
+                        </Box>
+                        : <Box/>
+                    }
+                </Paper> 
+            </div>
         </Container>
 
     </PageLayout>
