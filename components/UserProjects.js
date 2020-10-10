66382b5a1441c3b95d2f23f89d924aa3f88d47f5
Kevin Hsu <kevin.hsukaihao@gmail.com>
2020-10-10 15:33:41 -0700

    Kinda

--- components/UserProjects.js
+++ components/UserProjects.js
@@ -1,72 +1,52 @@
 import React, {useEffect, useState} from "react";
 import fb from "../src/firebase-config";
 import Box from "@material-ui/core/Box";
+import {useRouter} from "next/router";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import {Divider} from "@material-ui/core";
-import Paper from "@material-ui/core/Paper";
+import {Divider, Typography, Paper, Button} from "@material-ui/core";
 
-
-function navToProject(project_id){
-    router.push(`/projects/${project_id}`);
-}
-
-function PartCard ({projectInfo}) {
-    // const [displayName, setDisplayName] = useState(undefined)
-
-    // useEffect(() => {
-
-    //     fb.firestore().collection("profiles").doc(profileId)
-    //         .onSnapshot(function (doc) {
-    //             setDisplayName(doc.data().name)
-    //         });
-
-    // }, [])
-
-    return <ListItem onClick={navToProject}>
-        <ListItemText>
-            {projectInfo.title}
-        </ListItemText>
-        <Divider/>
-    </ListItem>
-}
 
 export default function UserProjects(props) {
     const [participantsData, setParticipantsData] = useState()
     const [isLoading, toggleLoading] = useState(true)
+    const router = useRouter();
+
+    function PartCard({projectInfo}) {
+        return <Paper variant="outlined" className="overflow-none">
+            <Box p={4}>
+                <ListItem>
+                    <ListItemText>
+                        <h2 className="text-3xl font-display">{projectInfo.title}</h2>
+                        <Typography variant="body1"> {projectInfo.description}</Typography>
+                    </ListItemText>
+                    <Divider/>
+                </ListItem>
+            </Box>
+        </Paper>
+    }
 
     useEffect(() => {
-
         fb.firestore().collection("projects").where("created_by", "==", props.profile_id).get()
             .then(function (querySnapshot){
                 var foo = []
                 querySnapshot.forEach(function (doc){
-                    console.log(doc.data());
-                    foo.push(doc.data())
+                    foo.push({id: doc.id, ...doc.data()})
                 });
                 setParticipantsData(foo);
                 toggleLoading(false)
             })
     }, [])
 
-    return <Paper variant="outlined">
-        <Box p={4}>
-            <h1 className="text-2xl font-display">Project Gallery: </h1>
+    return <div>
+            <h1 className="text-3xl font-display">Project Gallery: </h1>
             {!isLoading && <List>
-                <ListItem>
-                    <ListItemText>
-                        { participantsData.map(value => 
-                        <PartCard projectInfo={value}/>)
-                        }
-                    </ListItemText>
-                    <Divider/>
-                </ListItem>
-
+                { participantsData.map(value => 
+                <PartCard onClick={() => router.push(`/${value.id}`)} 
+                    projectInfo={value}/>)}
             </List>}
-
-        </Box>
-    </Paper>
+        </div>
 
 
 }
