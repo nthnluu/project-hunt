e5ee9251be33af200093ef74aaca951946cfeaaf
Nacho <71214325+nacho-0108@users.noreply.github.com>
2020-10-10 20:25:00 +0900

    Update [project_id].js

--- pages/[project_id].js
+++ pages/[project_id].js
@@ -11,12 +11,7 @@ import AuthContext from "../src/AuthContext";
 import LikeButton from "../components/LikeButton";
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import TagChip from "../components/TagChip";
-import Timeline from "@material-ui/lab/Timeline";
-import TimelineItem from "@material-ui/lab/TimelineItem";
-import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
-import TimelineConnector from "@material-ui/lab/TimelineConnector";
-import TimelineContent from "@material-ui/lab/TimelineContent";
-import TimelineDot from "@material-ui/lab/TimelineDot";
+import AlternateTimeline from "../components/Timeline";
 
 export default function () {
     const router = useRouter()
