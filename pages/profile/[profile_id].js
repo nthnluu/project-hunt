ad5afce3c0ec9fcc65ebd7210e3b09cacd0bd096
Kevin Hsu <kevin.hsukaihao@gmail.com>
2020-10-09 18:07:19 -0700

    Add follow user button and code cleanup

--- pages/profile/[profile_id].js
+++ pages/profile/[profile_id].js
@@ -5,7 +5,8 @@ import AuthContext from "../../src/AuthContext";
 import {useRouter} from "next/router";
 import PageLayout from "../../components/PageLayout";
 import ArrayInputPanel from "../../components/modals/NewProjectModal/ArrayInputPanel";
-import {Chip, Container, Paper, Box} from '@material-ui/core'
+import {Button, Chip, Container, Paper, Box} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles';
 
 import GitHubIcon from '@material-ui/icons/GitHub';
 import TwitterIcon from '@material-ui/icons/Twitter';
@@ -15,11 +16,18 @@ import ProtectedRoute from "../../components/ProtectedRoute";
 
 export default function () {
 
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            height: '100vh',
+        },
+    }));
+
     const router = useRouter();
     const {profile_id} = router.query;
 
     // Form Data
     const [pageData, setPageData] = useState()
+    const {sessionInfo} = useContext(AuthContext)
 
     // user: sessionInfo.uid,
     // githubUrl: githubLink, 
@@ -35,52 +43,62 @@ export default function () {
             });
     });
 
+    function followUser(){
+        var newFollowings = (pageData.followingUsers) ? pageData.followingUsers.push(profile_id) : [profile_id]; 
+        fb.firestore().collection("profiles").doc(sessionInfo.uid).set({
+            followingUsers: newFollowings
+        }, { merge: true });
+        alert(`Followed user ${pageData.name}!`);
+    }
+
     return <ProtectedRoute><PageLayout>
         <Container maxWidth="md" className="pt-4">
             <h1 className="mb-6 text-3xl font-display ">Basic Information</h1>
             <div className="space-y-4">
                 <Paper variant="outlined">
-                    {pageData
-                        ? <Box p={4}>
+                    {pageData &&
+                    <Box p={4}>
 
+                        <div className="grid grid-cols-2">
                             <h1 className="mb-6 text-3xl font-display ">{pageData.name}</h1>
+
+                            {(profile_id != sessionInfo.uid) && 
+                                    ((pageData.followingUsers && pageData.followingUsers.includes(profile_id))
+                                        ? <Button onClick={followUser} variant="contained" color="primary">Follow</Button> 
+                                        : <Button variant="contained" color="secondary">Following</Button>) }
+
                             <div> {`(Goes to ${pageData.institution})`} </div>
+                        </div>
 
-                            <div className="newProjectFormPanelGrid">
-                                <div className="grid grid-cols-4">
-                                    {pageData.githubUrl
-                                        ? <a href={pageData.githubUrl}><GitHubIcon fontSize="large"/></a>
-                                        : <p/>}
+                        <div className="newProjectFormPanelGrid">
+                            <div className="grid grid-cols-4">
+                                {pageData.githubUrl &&
+                                <a href={pageData.githubUrl}><GitHubIcon fontSize="large"/></a>}
 
-                                    {pageData.twitterUrl
-                                        ? <a href={pageData.twitterUrl}><TwitterIcon fontSize="large"/></a>
-                                        : <p/>}
+                                {pageData.twitterUrl && 
+                                    <a href={pageData.twitterUrl}><TwitterIcon fontSize="large"/></a>}
 
-                                    {pageData.websiteUrl
-                                        ? <a href={pageData.websiteUrl}><WebIcon fontSize="large"/></a>
-                                        : <p/>}
+                                {pageData.websiteUrl && 
+                                    <a href={pageData.websiteUrl}><WebIcon fontSize="large"/></a>}
 
-                                    {pageData.linkedinUrl
-                                        ? <a href={pageData.linkedinUrl}><LinkedInIcon fontSize="large"/></a>
-                                        : <p/>}
-                                </div>
-
-                                <div>
-                                    <h2 className="text-xl font-display">Bio: </h2>
-                                    <p>{pageData.bio}</p>
-                                </div>
-
-                                <div className="w-full">
-                                    {pageData.skills.map((skill, index) =>
-                                        <Chip key={skill.id}
-                                              label={skill.value} variant="outlined" className="mr-2 mb-2"/>)
-                                    }
-                                </div>
+                                {pageData.linkedinUrl &&
+                                    <a href={pageData.linkedinUrl}><LinkedInIcon fontSize="large"/></a>}
                             </div>
 
-                        </Box>
-                        : <Box/>
-                    }
+                            <div>
+                                <h2 className="text-xl font-display">Bio: </h2>
+                                <p>{pageData.bio}</p>
+                            </div>
+
+                            <div className="w-full">
+                                {pageData.skills.map((skill, index) =>
+                                <Chip key={skill.id}
+                                    label={skill.value} variant="outlined" className="mr-2 mb-2"/>)
+                                }
+                            </div>
+                        </div>
+
+                    </Box>}
                 </Paper>
             </div>
         </Container>
