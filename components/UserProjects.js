18809cc529e37ae6c8c73baf28a2055696c4ecea
Kevin Hsu <kevin.hsukaihao@gmail.com>
2020-10-10 15:43:04 -0700

    Complete Project gallery

--- components/UserProjects.js
+++ components/UserProjects.js
@@ -1,31 +1,21 @@
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import fb from "../src/firebase-config";
 import Box from "@material-ui/core/Box";
 import {useRouter} from "next/router";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import {Divider, Typography, Paper, Button} from "@material-ui/core";
+import {CardActionArea, Divider, Typography, Paper, Button} from "@material-ui/core";
+import LikeButton from "./LikeButton";
+import ImInButton from "./ImInButton";
+import AuthContext from "../src/AuthContext";
 
 
 export default function UserProjects(props) {
     const [participantsData, setParticipantsData] = useState()
     const [isLoading, toggleLoading] = useState(true)
     const router = useRouter();
-
-    function PartCard({projectInfo}) {
-        return <Paper variant="outlined" className="overflow-none">
-            <Box p={4}>
-                <ListItem>
-                    <ListItemText>
-                        <h2 className="text-3xl font-display">{projectInfo.title}</h2>
-                        <Typography variant="body1"> {projectInfo.description}</Typography>
-                    </ListItemText>
-                    <Divider/>
-                </ListItem>
-            </Box>
-        </Paper>
-    }
+    const {authState, sessionInfo} = useContext(AuthContext)
 
     useEffect(() => {
         fb.firestore().collection("projects").where("created_by", "==", props.profile_id).get()
@@ -40,13 +30,34 @@ export default function UserProjects(props) {
     }, [])
 
     return <div>
+        {!isLoading && 
+        <ul className="space-y-4 mb-8 mt-24">
             <h1 className="text-3xl font-display">Project Gallery: </h1>
-            {!isLoading && <List>
-                { participantsData.map(value => 
-                <PartCard onClick={() => router.push(`/${value.id}`)} 
-                    projectInfo={value}/>)}
-            </List>}
-        </div>
+            {participantsData.map(item => {
+                return <li key={item.id}>
+                    <Paper variant="outlined" key={item.id} className="overflow-none">
+                        <CardActionArea onClick={() => router.push(`/${item.id}`)}>
+                            <Box p={4}>
+                                <Typography variant="h5" component="h2"
+                                    gutterBottom>{item.title}</Typography>
+                                <Typography variant="body1">
+                                    {item.description}</Typography>
+                            </Box>
+                        </CardActionArea>
+                        <Box mx={3} mb={2}>
 
+                            <span className="mr-2">
+                                <LikeButton projectId={item.id}/>
+                            </span>
+
+                            {authState === 1 && ((item.created_by === sessionInfo.uid) ? null : <ImInButton projectId={item.id}/>)}
+
+                        </Box>
+                    </Paper>
+                </li>
+            })}
+        </ul>
+        }
+    </div>
 
 }
