a10be9a658a88014a7948b79ba8a7bf14a96996d
Nacho <71214325+nacho-0108@users.noreply.github.com>
2020-10-12 01:27:55 +0900

    edit

--- components/Timeline.js
+++ components/Timeline.js
@@ -1,11 +1,11 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React from 'react';
 import Timeline from '@material-ui/lab/Timeline';
 import TimelineItem from '@material-ui/lab/TimelineItem';
 import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
 import TimelineConnector from '@material-ui/lab/TimelineConnector';
 import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
-import fb from "../src/firebase-config";
+
 
 export default function OutlinedTimeline() {
   
